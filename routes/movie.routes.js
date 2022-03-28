f70489259b2acc9db2fcc8f4a000052ccd614436
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-03-28 15:20:38 -0400

    can add single movie to multiple lists now

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 const movies = require("../controllers/moviesController.js")
 
-router.get("/add-to-list", movies.addToList)
 router.get("/", movies.getByTitle)
 router.post("/", movies.create)
+router.post("/add-to-list", movies.addToList)
 router.get("/:id", movies.getById)
 router.get("/popular", movies.getPopular)
 
