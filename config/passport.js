262a3f1f0b8b16d430f2e12419fb864e056d20bf
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-05-03 17:43:31 -0400

    minor change to passport.js, unsure of it's usage still

--- config/passport.js
+++ config/passport.js
@@ -1,16 +1,19 @@
 require("dotenv").config()
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const db = require("../models");
+const User = db.Users;
 
 const accessToken = process.env.ACCESS_TOKEN_SECRET
 
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: accessToken
+    secretOrKey: accessToken,
+    // algorithms: ['RS256'] - is this needed to specify?
 }
 
 const strategy = new JwtStrategy(options, async (payload, done) => {
-    let user = await User.findByPk(id)
+    let user = await User.findByPk(payload.sub)
     try {
         if (user) {
           return done(null, user);
