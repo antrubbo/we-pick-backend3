5a41fa28fe348c3d62ac6a6715d12dd01b10987a
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-05-03 17:41:29 -0400

    renamed authenticate to generateAccessToken, also created authenticateToken.js

---
+++ helpers/authenticateToken.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken")
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader?.split(" ")[1]
+    if (!token) return res.sendStatus(401)
+
+    //this verifies that our token (ACCESS_TOKEN_SECRET) matches the incoming one
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+module.exports = { authenticateToken }
