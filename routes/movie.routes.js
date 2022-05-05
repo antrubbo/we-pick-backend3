40a3e0caf87eea890a3e6dc3a36fbf840e5315f9
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-05-05 15:22:33 -0400

    movie.routes cleanup

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -7,7 +7,9 @@ const { authenticateToken } = require("../helpers/authenticateToken")
 router.get("/", movies.getByTitle)
 // authenticateToken takes in the token in the Authorization Header and verifies it - not needed here, just for testing - it works!
 // still unsure of how passport fits into this scenario
-router.get("/all", authenticateToken, movies.getAllMovies)
+
+// insert authenticateToken after path
+router.get("/all", movies.getAllMovies)
 // router.post("/", movies.create)
 router.post("/add-to-list", movies.addToList)
 router.get("/popular", movies.getPopular)
